6a08a3d9dbd1e3cd9b86ca74da7639ea063d317f
Win Eiwwongcharoen <win.eiwwongcharoen@gmail.com>
2018-01-16 16:48:34 +0700

    Setup React

--- src/index.js
+++ src/index.js
@@ -1,1 +1,6 @@
-console.log('hello world')
+import React, { Component } from 'react'
+import { render } from 'react-dom'
+
+import App from './containers/App'
+
+render(<App />, document.getElementById('app'))
