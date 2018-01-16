53fcb34840f47c66cd38e223916cfa5f63590e9d
Win Eiwwongcharoen <win.eiwwongcharoen@gmail.com>
2018-01-16 16:56:51 +0700

    Update body styles

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,10 +1,12 @@
 import React, { Component } from 'react'
 
+import Card from '../components/Card/Card'
+
 export default class App extends Component {
   render() {
     return (
       <div>
-        <h1>App</h1>
+        <Card />
       </div>
     )
   }
